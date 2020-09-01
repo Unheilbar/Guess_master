905e7bae81cbdd20b02df6b82e16d12739d2e14e
unheilbar <vladimiramnell@gmail.com>
2020-09-01 21:30:57 +1000

    2nd

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const request = require('request-promise')
+const artistids = require('./utils/artist-ids')
 
 const app = express()
 const server =  http.createServer(app)
 const io = socketio(server)
-const zalupa = {name:'zalupa'}
-let id = 0
+
+
+
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
 
 //Run when client connects
 io.on('connection', socket => {
@@ -20,7 +24,6 @@ io.on('connection', socket => {
     //Broadcast when connect
     socket.broadcast.emit('message', 'A user has joined the chat')
 
-    socket.emit('freak', newName())
 
     //Runs when client disconnects
     socket.on('disconnect', socket => {
@@ -28,12 +31,60 @@ io.on('connection', socket => {
     })
 })
 
-function newName() {
-    const newName = {name:zalupa.name}
-    newName.name+=id
-    id++
-    return newName
+
+class Game {
+    constructor(name, ids) {
+        this.name = name
+        this.ids  = ids
+    }
+
+    songPlayed = 0
+
+    generateUri() {
+        let rand = Math.random()*(this.ids.length-0.5)
+        let uri = 'https://itunes.apple.com/lookup?id=' + this.ids[Math.round(rand)] + '&entity=song&limit=5&sort=recent'
+        this.currentUri = uri
+    }
+
+    parseJson(json) {
+        json.JSO
+    }
+
+    loadNextTrack() {
+        this.generateUri()
+        const options = {
+            method:'Get',
+            uri:this.currentUri,
+            json:true
+        }
+
+        request(options).
+            then(res => {
+                this.initiateCurrentTrack(res)
+                //console.log(res)
+                this.showRes()
+            })
+    }
+
+    initiateCurrentTrack(response) {
+        const songNumber = Math.round(Math.random()*5)+1
+        this.artistName = response.results[songNumber].artistName
+        this.trackName = response.results[songNumber].trackName
+    }
+
+    showRes(){
+        setTimeout(() => {
+            this.songPlayed++
+            console.log(this.artistName, this.trackName)
+            this.loadNextTrack()
+        }
+    , 5000)
+    }
+
 }
+const room = new Game('trash', artistids)
+
+room.loadNextTrack()
 
 const PORT = 3000
 
